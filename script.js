53c6d41bf209e26131fd14c51d49ca1623dcd147
Kevin <litchfield.kevin+gs@gmail.com>
2015-01-15 15:22:06 -0800

    Can change background color

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
 var cells = document.querySelectorAll("#grid .row div");
 var paletteCells = document.querySelectorAll("#palette .row div");
-var currentColorDisplay = document.querySelector("#current-color");
+var colorDisplays = document.querySelectorAll(".color-display");
 
-var gridBackgroundColor = "white";
+
+var backgroundColor = "white";
 var paintbrushColor = "white";
+
+
+
+
+
+var paintbrushColorDisplay = document.querySelector("#paintbrush-color");
+var backgroundColorDisplay = document.querySelector("#background-color");
+
+var colorDisplaySelected = paintbrushColorDisplay;
+
+
+
+
+
+paintbrushColorDisplay.onclick = selectPaintbrushColorDisplay;
+backgroundColorDisplay.onclick = selectBackgroundColorDisplay;
+
+
+
+function selectPaintbrushColorDisplay(){
+  if (this.className.indexOf("selected") === -1){
+    this.className = this.className + " selected";
+    colorDisplaySelected = paintbrushColorDisplay;
+    unselect(backgroundColorDisplay);
+  }
+}
+
+function selectBackgroundColorDisplay(){
+  if (this.className.indexOf("selected") === -1){
+    this.className = this.className + " selected";
+    colorDisplaySelected = backgroundColorDisplay;
+    unselect(paintbrushColorDisplay);
+  }
+}
+
+function unselect(colorDisplay){
+  if (colorDisplay.className.indexOf("selected") !== -1){
+    colorDisplay.className = colorDisplay.className.replace("selected", "");
+  }
+}
+
+
 
 
 for (var i = 0; i < cells.length; i++){
@@ -12,16 +55,18 @@ for (var i = 0; i < cells.length; i++){
 
 
 function colorCell(){
-  if (((this.className).indexOf("colored")) === -1) {
-    this.className = this.className + " colored";
-    this.style.backgroundColor = paintbrushColor;
-  } else if (this.style.backgroundColor !== paintbrushColor){
-    this.style.backgroundColor = paintbrushColor;
-  } else {
-    var currentClasses = this.className;
-    var newCurrentClasses = currentClasses.replace("colored", "");
-    this.className = newCurrentClasses;
-    this.style.backgroundColor = gridBackgroundColor;
+  if (colorDisplaySelected === paintbrushColorDisplay){
+    if (((this.className).indexOf("colored")) === -1) {
+      this.className = this.className + " colored";
+      this.style.backgroundColor = paintbrushColor;
+    } else if (this.style.backgroundColor !== paintbrushColor){
+      this.style.backgroundColor = paintbrushColor;
+    } else {
+      var currentClasses = this.className;
+      var newCurrentClasses = currentClasses.replace("colored", "");
+      this.className = newCurrentClasses;
+      this.style.backgroundColor = backgroundColor;
+    }
   }
 }
 
@@ -31,8 +76,18 @@ function fillPalette(){
   for (var i = 0; i < paletteCells.length; i++){
     paletteCells[i].style.backgroundColor = colors[i];
     paletteCells[i].onclick = function(){
-      paintbrushColor = this.style.backgroundColor;
-      currentColorDisplay.style.backgroundColor = this.style.backgroundColor;
+      if (colorDisplaySelected === paintbrushColorDisplay){
+        paintbrushColor = this.style.backgroundColor;
+        paintbrushColorDisplay.style.backgroundColor = this.style.backgroundColor;
+      } else {
+        backgroundColor = this.style.backgroundColor;
+        backgroundColorDisplay.style.backgroundColor = this.style.backgroundColor;
+        for (var i = 0; i < cells.length; i++){
+          if (cells[i].className.indexOf("colored") === -1){
+            cells[i].style.backgroundColor = backgroundColor;
+          }
+        }
+      }
     }
   }
 }
